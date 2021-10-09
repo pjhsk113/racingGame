7c4b9ccdfe640740f910a9fd31d46edf90a3f03b
JanghoPark <pjhsk113@naver.com>
2021-10-09 18:32:05 +0900

    feat: 자동차 이동 전략 구현체 생성

---
+++ src/main/java/racinggame/domain/CarMoveStrategy.java
@@ -0,0 +1,13 @@
+package racinggame.domain;
+
+import newtstep.utils.Randoms;
+import racinggame.constant.GameRules;
+
+public class CarMoveStrategy implements MoveStrategy {
+
+    @Override
+    public boolean moveAble() {
+        int randomNumber = Randoms.pickNumberInRange(GameRules.START_INCLUSIVE.getValue(), GameRules.END_INCLUSIVE.getValue());
+        return randomNumber >= GameRules.MOVEABLE.getValue();
+    }
+}
