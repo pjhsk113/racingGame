deb7e89dbfca7b02ab40b4025e5c6afe17e8ec9b
JanghoPark <pjhsk113@naver.com>
2021-10-10 18:33:21 +0900

    test: Car 클래스 테스트 추가

---
+++ src/test/java/racinggame/domain/CarTest.java
@@ -0,0 +1,48 @@
+package racinggame.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class CarTest {
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = Car.from("a");
+    }
+
+    @DisplayName("값이 4이상일때만 1칸 전진한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"4 : 1", "5 : 1", "6 : 1", "7 : 1", "2 : 0", "3 : 0" }, delimiter = ':')
+    void carMoveTest(int moveNumber, int expectPosition) {
+        TestMoveStrategy moveStrategy = new TestMoveStrategy();
+        moveStrategy.inputNumber(moveNumber);
+        car.move(moveStrategy);
+
+        assertThat(car.getPosition()).isEqualTo(expectPosition);
+    }
+
+    @DisplayName("자동차 이름이 5글자를 초과하면 IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"aaaaaaaa", "bbbbbbb", "cccccc"})
+    void carNameLengthTest(String inputName) {
+        assertThatThrownBy(() -> {
+            Car.from(inputName);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차 이름에 빈 값이 입력되면 IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "", "     "})
+    void carNameBlankTest(String inputName) {
+        assertThatThrownBy(() -> {
+            Car.from(inputName);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+}
