f1ae75a986077712889775cf91e9824c93341afb
JanghoPark <pjhsk113@naver.com>
2021-10-10 18:28:47 +0900

    feat: Cars - 레이싱 게임 승자를 반환하는 로직 추가

--- src/main/java/racinggame/domain/Cars.java
+++ src/main/java/racinggame/domain/Cars.java
@@ -12,7 +12,7 @@ public class Cars {
         this.cars = cars;
     }
 
-    public static Cars of(List<Car> carNames) throws IllegalArgumentException {
+    public static Cars from(List<Car> carNames) {
         return new Cars(carNames);
     }
 
@@ -27,9 +27,32 @@ public class Cars {
         List<Car> cars = new ArrayList<>();
         for (String carName : carNames) {
             validateCarName(cars, carName);
-            cars.add(Car.of(carName));
+            cars.add(Car.from(carName));
         }
         return cars;
+    }
+
+    public List<Car> getWinner() {
+        List<Car> winners = new ArrayList<>();
+        for (Car car : cars) {
+            winners = isWinner(winners, car);
+        }
+        return winners;
+    }
+
+    private List<Car> isWinner(List<Car> winners, Car car)  {
+        if (car.getPosition() == getMaxPosition()) {
+            winners.add(car);
+        }
+        return winners;
+    }
+
+    private int getMaxPosition() {
+        int maxPostion = Integer.MIN_VALUE;
+        for (Car car : cars) {
+            maxPostion = Math.max(maxPostion, car.getPosition());
+        }
+        return maxPostion;
     }
 
     public Car valueOf(int index) {
