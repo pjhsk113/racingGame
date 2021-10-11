4a57bc660f537b045b1636aaeac5f01158044db9
JanghoPark <pjhsk113@naver.com>
2021-10-11 16:21:43 +0900

    refactor: 주석 제거

--- src/main/java/racinggame/domain/RacingGame.java
+++ src/main/java/racinggame/domain/RacingGame.java
@@ -5,15 +5,11 @@ import racinggame.view.OutputView;
 
 public class RacingGame {
     public void play() {
-        // 자동차 이름 입력
         Cars cars = Cars.from(InputView.inputCarName());
 
-        // 라운드 입력
         Round round = Round.from(InputView.inputRound());
-        // 라운드 별 결과
         round.roundResult(cars);
 
-        // 레이싱 우승자 출력
         OutputView.printWinner(cars.getWinner());
     }
 }
